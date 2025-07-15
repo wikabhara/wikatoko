547f92c68f65eea3762c1a0b94da6a2e19078449
wikabhara <wikabhara@gmail.com>
2025-07-16 00:51:39 +0700

    feat: firebase @RegisterPage

--- src/pages/RegisterPage.jsx
+++ src/pages/RegisterPage.jsx
@@ -1,19 +1,35 @@
-import React from "react";
+import { useState } from "react";
 
 export default function RegisterPage() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  async function handleRegister(e) {
+    e.preventDefault();
+    console.log(email, password);
+  }
+
   return (
     <div>
       <h1>Register Page</h1>
-      <form action="">
+      <form onSubmit={handleRegister} action="">
         <div>
           <label htmlFor="">Email</label>
           <br />
-          <input type="text" />
+          <input
+            type="text"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
         </div>
         <div>
           <label htmlFor="">Password</label>
           <br />
-          <input type="password" />
+          <input
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
         </div>
         <button>Register</button>
       </form>
