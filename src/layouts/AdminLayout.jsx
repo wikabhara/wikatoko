213a16e1f5cb0339fe61835853d13304f5cfd654
wikabhara <wikabhara@gmail.com>
2025-07-15 22:42:21 +0700

    feat: routing auth login and register

---
+++ src/layouts/AdminLayout.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { Outlet } from "react-router";
+
+export default function AdminLayout() {
+  return (
+    <>
+      {/* navbar */}
+      <header>Admin Side</header>
+      {/* navbar */}
+      <Outlet />
+    </>
+  );
+}
