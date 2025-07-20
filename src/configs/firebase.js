ccc5714290c7f20e2cd54e6ea32a5c84e6e6d928
wikabhara <wikabhara@gmail.com>
2025-07-20 17:21:28 +0700

    add: .env save firebase.js

--- src/configs/firebase.js
+++ src/configs/firebase.js
@@ -8,19 +8,23 @@ import { getAuth } from "firebase/auth";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyAApl5ScxML20pKoCgJw4VyEv26qxBJQ-o",
-  authDomain: "p2-gc01-c78be.firebaseapp.com",
-  projectId: "p2-gc01-c78be",
-  storageBucket: "p2-gc01-c78be.firebasestorage.app",
-  messagingSenderId: "812381491840",
-  appId: "1:812381491840:web:ae8be854b56f3622a20c65",
-  measurementId: "G-CEH81DH4E0",
-};
-
-const firebaseConfig = {
   apiKey: import.meta.env.VITE_API_KEY,
   authDomain: import.meta.env.VITE_AUTH_DOMAIN,
   projectId: import.meta.env.VITE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_APP_ID,
+  measurementId: import.meta.env.VITE_MEASUREMENT_ID,
+};
+// const firebaseConfig = {
+//   apiKey: "AIzaSyAApl5ScxML20pKoCgJw4VyEv26qxBJQ-o",
+//   authDomain: "p2-gc01-c78be.firebaseapp.com",
+//   projectId: "p2-gc01-c78be",
+//   storageBucket: "p2-gc01-c78be.firebasestorage.app",
+//   messagingSenderId: "812381491840",
+//   appId: "1:812381491840:web:ae8be854b56f3622a20c65",
+//   measurementId: "G-CEH81DH4E0",
+// };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
