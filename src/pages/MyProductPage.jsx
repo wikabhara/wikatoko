be797f3016d91c717770f5e1b255a4a0fb6ea9f3
wikabhara <wikabhara@gmail.com>
2025-07-23 18:52:42 +0700

    feat: firestore create

--- src/pages/MyProductPage.jsx
+++ src/pages/MyProductPage.jsx
@@ -1,8 +1,33 @@
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import { AuthContext } from "../contexts/AuthContext";
+import { collection, addDoc } from "firebase/firestore";
+import { db } from "../configs/firebase";
+import { useNavigate } from "react-router";
 
 export default function MyProductPage() {
   const { user } = useContext(AuthContext);
+  const [Name, setName] = useState("");
+  const [ImageUrl, setImageUrl] = useState("");
+  const [Price, setPrice] = useState(0);
+  const navigate = useNavigate();
+
+  async function submitProduct(e) {
+    e.preventDefault();
+    try {
+      const docRef = await addDoc(collection(db, "products"), {
+        Name: Name,
+        ImageUrl: ImageUrl,
+        Price: Price,
+      });
+      console.log(docRef);
+      console.log("produk berhasil ditambahkan", Name);
+      navigate("/");
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  useEffect(() => {}, []);
   return (
     <>
       <div className="min-h-[80vh] flex items-center justify-center bg-base-100">
@@ -17,23 +42,35 @@ export default function MyProductPage() {
               Add Product
             </h1>
 
-            <form action="">
+            <form onSubmit={submitProduct} action="">
               <div>
                 <label htmlFor="">Product Name</label>
                 <br />
-                <input type="text" />
+                <input
+                  type="text"
+                  value={Name}
+                  onChange={(e) => setName(e.target.value)}
+                />
               </div>
               <div>
                 <label htmlFor="">Image URL</label>
                 <br />
-                <input type="text" />
+                <input
+                  type="text"
+                  value={ImageUrl}
+                  onChange={(e) => setImageUrl(e.target.value)}
+                />
               </div>
               <div>
                 <label htmlFor="">Price</label>
                 <br />
-                <input type="text" />
+                <input
+                  type="number"
+                  value={Price}
+                  onChange={(e) => setPrice(+e.target.value)}
+                />
               </div>
-              <button type="button" className="btn">
+              <button type="submit" className="btn">
                 Submit
               </button>
             </form>
